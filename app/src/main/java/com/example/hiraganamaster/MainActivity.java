008a721fd4641e7bad9502267333bfbf8328064d
japkidzsola <suzukipontzsolt@gmail.com>
2020-03-20 02:03:53 +0100

    sikeres notification

--- app/src/main/java/com/example/hiraganamaster/MainActivity.java
+++ app/src/main/java/com/example/hiraganamaster/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.hiraganamaster;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.example.hiraganamaster.ui.hiraganamasterhome.HiraganaMasterHomeFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.os.CountDownTimer;
 import android.view.View;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,9 +29,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 
+import static com.example.hiraganamaster.Notification.CHANNEL_1_ID;
+
 public class MainActivity extends AppCompatActivity {
 
+    private long notificationTimer = 20000;
+    private CountDownTimer countDownTimer;
+    private NotificationManagerCompat notificationManager;
     private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +65,37 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        notificationManager = NotificationManagerCompat.from(this);
+    }
+
+    public void startTimer()
+    {
+        countDownTimer = new CountDownTimer(notificationTimer, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                String title = getString(R.string.notification_title);
+                String message = getString(R.string.notification_message);
+
+                Intent activityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                PendingIntent contentIntent = PendingIntent.getActivity(getApplicationContext(),0,activityIntent,0);
+
+                Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_1_ID).setSmallIcon(R.drawable.ic_learn_black_24dp)
+                        .setContentTitle(title).setContentText(message).setPriority(NotificationCompat.PRIORITY_DEFAULT).setCategory(NotificationCompat.CATEGORY_MESSAGE).setColor(Color.BLUE)
+                        .setContentIntent(contentIntent).build();
+
+                notificationManager.notify(1, notification);
+            }
+        }.start();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        startTimer();
     }
 
     @Override
---
+++ app/src/main/java/com/example/hiraganamaster/Notification.java
@@ -0,0 +1,32 @@
+package com.example.hiraganamaster;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class Notification extends Application {
+    public static final String CHANNEL_1_ID = "appnotification";
+
+    public void onCreate()
+    {
+        super.onCreate();
+
+        createNotificationChannels();
+    }
+    private void createNotificationChannels()
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Hiragana Master Notification",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel1.setDescription("This is Hiragana Master's notification");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+        }
+    }
+}
